b29ead0ae8cb484294e75704f2b11adffaa41497
DandinPower <tomhot246@gmail.com>
2023-02-19 12:48:40 +0800

    speech recognition version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import io from 'socket.io-client';
 import ReactPlayer from 'react-player';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
-import axios from 'axios';
 
 const SERVER_HOST = process.env.REACT_APP_SERVER_HOST;
 const socket = io.connect(SERVER_HOST);
@@ -12,20 +11,16 @@ function App() {
   const [audioChunks, setAudioChunks] = useState([]);
   const [playing, setPlaying] = useState(false);
   const [url, setUrl] = useState(null);
-  const [textInput, setTextInput] = useState('');
   const [selectedOption, setSelectedOption] = useState('en-US-DavisNeural');
-  const [recording, setRecording] = useState(false);
-  const [recordedAudioChunks, setRecordedAudioChunks] = useState([]);
-  const [stream, setStream] = useState(null);
+  const [isRecording, setIsRecording] = useState(false);
+  const [transcript, setTranscript] = useState('');
 
-  const requestAudioData = useCallback(async(text, option, audioBlob) => {
+  const requestAudioData = useCallback((text, option) => {
     // Clear the current audio chunks
-    console.log(`in request`)
-    console.log(audioBlob)
-    
     setAudioChunks([]);
+
     // Request audio data from the server
-    socket.emit('request-audio', text, option, audioBlob);
+    socket.emit('request-audio', text, option);
 
     // Receive audio data from the server
     const audioChunkHandler = (chunk) => {
@@ -50,85 +45,59 @@ function App() {
     setUrl(audioUrl);
   }, [audioChunks]);
 
-  useEffect(() => {
-    const audioBlob = new Blob(recordedAudioChunks, { type: 'audio/wav' });
-    console.log(`in use Effect`)
-    console.log(audioBlob)
-    if (playing) {
-      requestAudioData(textInput, selectedOption, audioBlob);
-    }
-  }, [recordedAudioChunks]);
-
-
   const handleSelectChange = (event) => {
     setSelectedOption(event.target.value);
   }
 
-  const handleStartRecording = async () => {
-    try {
-      const constraints = {
-        audio: {
-          sampleRate: 16000,
-          channelCount: 1,
-          bitrate: 256000,
-        },
-      };
-      const newStream = await navigator.mediaDevices.getUserMedia(constraints);
-      setRecording(true);
-      setStream(newStream);
-      setRecordedAudioChunks([]);
-      const mediaRecorder = new MediaRecorder(newStream);
-      mediaRecorder.addEventListener('dataavailable', handleDataAvailable);
-      mediaRecorder.start();
-    } catch (error) {
-      console.error(error);
-    }
+  const startRecording = () => {
+    setIsRecording(true);
+
+    const recognition = new window.webkitSpeechRecognition();
+    recognition.lang = 'en-US';
+
+    recognition.onresult = (event) => {
+      const last = event.results.length - 1;
+      const spoken = event.results[last][0].transcript;
+      requestAudioData(spoken, selectedOption);
+      setPlaying(true);
+      setTranscript(spoken);
+    };
+
+    recognition.start();
   };
 
-  const handleStopRecording = async () => {
-    setRecording(false);
-    stream.getTracks().forEach((track) => track.stop())
-    setPlaying(true);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (recording) {
-      handleStopRecording();
-    } else {
-      handleStartRecording();
-    }
-  };
-
-  const handleDataAvailable = (event) => {
-    if (event.data.size > 0) {
-      setRecordedAudioChunks((chunks) => [...chunks, event.data]);
-    }
+  const stopRecording = () => {
+    setIsRecording(false);
   };
 
   return (
     <Container className="my-4">
       <Row>
         <Col>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group controlId="textInput">
-                <Form.Label>Enter Text</Form.Label>
-                <Form.Control type="text" value={textInput} onChange={(e) => setTextInput(e.target.value)} />
-              </Form.Group>
-
-              <Form.Group controlId="selectOption">
-                <Form.Label>Select a Voice</Form.Label>
-                <Form.Control as="select" value={selectedOption} onChange={handleSelectChange}>
-                  <option value="en-US-DavisNeural">en-US-DavisNeural</option>
-                  <option value="en-US-CoraNeural">en-US-CoraNeural</option>
-                  <option value="en-US-AshleyNeural">en-US-AshleyNeural</option>
-                </Form.Control>
-              </Form.Group>
-              <br/>
-              <Button variant="primary" type="submit">{recording ? 'Stop Recording' : 'Record and Play'}</Button>
-              </Form>
+          <Form>
+            <Form.Group controlId="selectOption">
+              <Form.Label>Select a Voice</Form.Label>
+              <Form.Control as="select" value={selectedOption} onChange={handleSelectChange}>
+                <option value="en-US-DavisNeural">en-US-DavisNeural</option>
+                <option value="en-US-CoraNeural">en-US-CoraNeural</option>
+                <option value="en-US-AshleyNeural">en-US-AshleyNeural</option>
+              </Form.Control>
+            </Form.Group>
+          </Form>
         </Col>
       </Row>
+      <Row className='my-4'>
+        <Col>
+          <Button onClick={startRecording} disabled={isRecording}>
+          Start Recording
+          </Button>
+          <Button onClick={stopRecording} disabled={!isRecording}>
+            Stop Recording
+          </Button>
+        <p>Transcript: {transcript}</p>
+        </Col>
+      </Row>
+
       <Row className="mt-4">
         <Col>
           {url && (
